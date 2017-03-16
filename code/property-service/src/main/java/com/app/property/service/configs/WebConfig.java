a94b440501458f1101988df3dbd83626598a3ed2
mumbai272 <vinayak.s.mumbai@gmail.com>
2017-03-17 01:58:40 +0530

    seup

--- code/property-service/src/main/java/com/app/property/service/configs/WebConfig.java
+++ code/property-service/src/main/java/com/app/property/service/configs/WebConfig.java
@@ -3,24 +3,16 @@
 //============================================================
 package com.app.property.service.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc //mvc:annotation-driven
 @Configuration
 @ComponentScan({ "com.app.property.service.*" })
 public class WebConfig extends WebMvcConfigurerAdapter {
  
-    @Bean
-    public InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("WEB-INF/");
-        viewResolver.setSuffix(".html");
-        return viewResolver;
-    }
+    
    
 }
